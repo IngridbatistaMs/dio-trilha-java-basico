a3424b78dda0b36e310dbaa833e6878217ca950b
ingridbatistams <ingridmarianb92@gmail.com>
2024-05-09 14:22:22 -0300

    Testando tipos de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,50 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        
+        System.out.println(resultado);
+
+        String nome1 = "Ingrid";
+        String nome2 = new String( "Ingrid");
+
+        System.out.println(nome1.equals(nome2));
+
+        int num1 = 1;
+        int num2 = 2;
+
+        boolean simNao = num1 == num2;
+
+        if (num1 < num2) {
+            System.out.println("a condição é verdadeira");
+        }
+
+        System.out.println("Número1 é igual do Número2? " + simNao);
+
+        simNao = num1 != num2;
+
+        System.out.println("Número1 é diferente do Número2? " + simNao);
+
+        simNao = num1 > num2;
+
+        System.out.println("Número1 é maior que o Número2? " + simNao);*/
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condições são verdadeiras");
+            
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições são verdadeiras");
+            
+        }
+        System.out.println("fim");
+           
+    }
+
+}
